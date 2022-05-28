4683404daf275c0d1a92a2eb2eac9e87fab160e1
Kevin Williams <kevin.williams@kevinwilliams.dev>
2022-05-28 17:32:20 -0500

    Updates
    Split extensions from software install, added profile script and vs code settings

---
+++ SoftwareInstall.ps1
@@ -0,0 +1,43 @@
+# Utilities
+winget install Microsoft.Powertoys --silent
+winget install RARLab.WinRAR --silent
+winget install RaspberryPiFoundation.RaspberryPiImager --silent
+
+# Browsers
+winget install BraveSoftware.BraveBrowser --silent
+winget install Mozilla.Firefox --silent
+
+# Source Control
+winget install Git.Git --silent
+winget install Axosoft.GitKraken --silent
+
+# DevTools
+winget install PuTTY.PuTTY --silent
+winget install Postman.Postman --silent
+winget install Microsoft.AzureCLI --silent
+
+# SDKs & Frameworks
+winget install OpenJS.NodeJS --silent
+winget install Python.Python.3 --silent
+winget install Microsoft.DotNet.SDK.5 --silent
+winget install Microsoft.DotNet.SDK.6 --silent
+
+# IDEs
+winget install Notepad++.Notepad++ --silent
+winget install Microsoft.VisualStudioCode --silent
+winget install Microsoft.VisualStudio.2022.Community --silent
+
+# Databases
+winget install Microsoft.AzureDataStudio --silent
+winget install Microsoft.AzureCosmosEmulator --silent
+winget install Microsoft.SQLServer.2019.Developer --silent
+
+# Containers
+winget install Docker.DockerDesktop --silent
+
+# Misc
+winget install OBSProject.OBSStudio --silent
+winget install Adobe.Acrobat.Reader.64-bit --silent
+winget install Discord.Discord --silent
+winget install JGraph.Draw --silent
+
---
+++ VSCodeExtensions.ps1
@@ -0,0 +1,14 @@
+code --install-extension ms-python.python --force
+code --install-extension ms-vscode.cpptools --force
+code --install-extension ms-dotnettools.csharp --force
+
+code --install-extension dart-code.flutter --force
+code --install-extension ms-dotnettools.vscode-dotnet-runtime --force
+
+code --install-extension ms-vscode.vscode-node-azure-pack --force
+code --install-extension ms-vsliveshare.vsliveshare --force
+
+code --install-extension esbenp.prettier-vscode --force
+
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.remote-containers
