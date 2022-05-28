f2daa1768bf0742a67aef13caaae99429acabc7c
Kevin Williams <44476916+Demetrioz@users.noreply.github.com>
2022-05-28 14:05:33 -0500

    Initial Commit
    Initial installation script

---
+++ WindowsSetup.ps1
@@ -0,0 +1,43 @@
+# Utilities
+winget install Microsoft.Powertoys
+winget install Microsoft.WindowsTerminal
+winget install RARLab.WinRAR
+winget install RaspberryPiFoundation.RaspberryPiImager
+
+# Browsers
+winget install BraveSoftware.BraveBrowser\
+winget install Mozilla.Firefox
+
+# Source Control
+winget install Git.Git
+winget install Axosoft.GitKraken
+
+# DevTools
+winget install PuTTY.PuTTY
+winget install Postman.Postman
+winget install Microsoft.AzureCLI
+
+# SDKs & Frameworks
+winget install OpenJS.NodeJS
+winget install Python.Python.3
+winget install Microsoft.DotNet.SDK.5
+winget install Microsoft.DotNet.SDK.6
+
+# IDEs
+winget install Notepad++.Notepad++
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudio.2022.Community
+
+# Databases
+winget install Azure Data Studio
+winget install Microsoft.AzureCosmosEmulator
+winget install Microsoft.SQLServer.2019.Developer
+
+# Containers
+winget install Docker.DockerDesktop
+
+# Misc
+winget install OBSProject.OBSStudio\
+winget install Adobe.Acrobat.Reader.64-bit
+winget install Discord.Discord
+winget install JGraph.Draw
